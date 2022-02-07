5b89efddf5757cbe12924f7fd104868874460b7c
maximiliano ibarra <maxi.8379@gmail.com>
2022-02-06 23:30:58 -0300

    .

--- src/config.js
+++ src/config.js
@@ -2,15 +2,15 @@ export default {
     fileSystem: {
         path: './DB'
     },
-    mongodb: {
-        cnxStr: 'mongodb://localhost/proyectofinal',
-        options: {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            serverSelectionTimeoutMS: 5000,
-        }
-    },
+    // mongodb: {
+    //     cnxStr: 'mongodb://localhost/proyectofinal',
+    //     options: {
+    //         useNewUrlParser: true,
+    //         useUnifiedTopology: true,
+    //         useCreateIndex: true,
+    //         serverSelectionTimeoutMS: 5000,
+    //     }
+    // },
     sqlite3: {
         client: 'sqlite3',
         connection: {
